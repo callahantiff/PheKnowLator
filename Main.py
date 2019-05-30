7bd3c4be3837a561bb97166b2e98c50231973501
callahantiff <callahantiff@gmail.com>
2019-05-29 23:29:59 -0600

    refactored code
    - combined data download class and inherited children classes into single script
    - improved metadata creation file

--- Main.py
+++ Main.py
@@ -1,16 +1,14 @@
 ##########################################################################################
 # main.py
 # Purpose: script to download OWL ontology files and store source metadata
-# version 1.1.0
-# date: 07.28.2018
 # Python 3.6.2
 ##########################################################################################
 
 
 # import needed libraries
 import argparse
-import scripts.python.DataSources_ontologies
-import scripts.python.DataSources_data
+
+import scripts.python.DataSources
 import scripts.python.EdgeDictionary
 from scripts.python.KnowledgeGraph import *
 
@@ -23,12 +21,10 @@ from scripts.python.KnowledgeGraph import *
 '''
 
 
-
-
 def main():
-    parser = argparse.ArgumentParser(description='OpenBioGraph: This program builds a biomedical knowledge graph '
+    parser = argparse.ArgumentParser(description='PheKnowLator: This program builds a biomedical knowledge graph '
                                                  'using Open Biomedical Ontologies and other sources of open '
-                                                 'biomedical data. Built on Semantic Web Technologies, the programs '
+                                                 'biomedical data. Built on Semantic Web Technologies, the program '
                                                  'takes the inputs specified below and outputs')
     parser.add_argument('-o', '--onts', help='name/path to text file containing ontologies', required=True)
     parser.add_argument('-c', '--cls', help='name/path to text file containing class sources', required=True)
@@ -41,8 +37,8 @@ def main():
     # READ IN DATA #
     ######################
     # read in ontologies
-    ont = scripts.python.DataSources_ontologies.OntData(args.onts)
-    ont = scripts.python.DataSources_ontologies.OntData("resources/ontology_source_list.txt")
+    # ont = scripts.python.drafts.DataSources_ontologies.OntData(args.onts)
+    ont = scripts.python.DataSources.OntData('resources/ontology_source_list.txt')
     ont.file_parser()
     ont.get_data_type()
     ont.get_source_list()
@@ -53,8 +49,8 @@ def main():
     ont.write_source_metadata()
 
     # read in class data
-    cls = scripts.python.DataSources_data.Data(args.cls)
-    cls = scripts.python.DataSources_data.Data("resources/class_source_list.txt")
+    # cls = scripts.python.DataSources_data.Data(args.cls)
+    cls = scripts.python.DataSources.Data('resources/class_source_list.txt')
     cls.file_parser()
     cls.get_data_type()
     cls.get_source_list()
@@ -65,8 +61,8 @@ def main():
     cls.write_source_metadata()
 
     # read in class and instance data
-    inst = scripts.python.DataSources_data.Data(args.inst)
-    inst = scripts.python.DataSources_data.Data("resources/instance_source_list.txt")
+    inst = scripts.python.drafts.DataSources_data.Data(args.inst)
+    inst = scripts.python.drafts.DataSources_data.Data('resources/instance_source_list.txt')
     inst.file_parser()
     inst.get_data_type()
     inst.get_source_list()
@@ -103,15 +99,15 @@ def main():
     # CONSTRUCT KG #
     ######################
     # merge ontologies - function from scripts.python.KnowledgeGraph
-    OntologyMerger("./resources/ontologies/go_with_imports.owl",
-                   "./resources/ontologies/hp_with_imports.owl",
+    OntologyMerger('./resources/ontologies/go_with_imports.owl',
+                   './resources/ontologies/hp_with_imports.owl',
                    './resources/ontologies/generated_ontologies/hp+go')
 
     # create class-instance and class instance-instance edges
     edge_dict = source_dict_cls
     graph_output = './resources/ontologies/generated_ontologies/hp+go_merged_instances.owl'
     iri_map_output = './resources/graphs/class_instance_map.json'
-    input_graph = "./resources/ontologies/generated_ontologies/hp+go_merged.owl"
+    input_graph = './resources/ontologies/generated_ontologies/hp+go_merged.owl'
     KG = classEdges(edge_dict, graph_output, input_graph, iri_map_output)
 
     # create instance-instance edges in KG
@@ -126,7 +122,7 @@ def main():
     removeDisointness(graph, output)
 
     # deductively close graph
-    graph = 'resources/ontologies/generated_ontologies/hp+go_merged_instances_full_nodisjoint.owl'
+    graph = 'hp+go_merged_instances_full_nodisjoint.owl'
     output = 'resources/ontologies/generated_ontologies/hp+go_merged_instances_full_nodisjoint'
     reasoner = 'elk'
     CloseGraph(graph, reasoner, output)
@@ -150,3 +146,202 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+# import needed libraries
+from rdflib import Namespace
+from rdflib import Graph
+from rdflib import URIRef
+import re
+import urllib
+import glob
+
+
+def edge_loader(updated_edge):
+    edit_list = []
+
+    for idx, link in updated_edge.items():
+        if len(link) > 1:
+            if idx == '0':
+                for i in link:
+                    edit_list.append((i, updated_edge['1'][0], updated_edge['2'][0]))
+            elif idx == '1':
+                for j in link:
+                    edit_list.append((updated_edge['0'][0], j, updated_edge['2'][0]))
+            else:
+                for k in link:
+                    edit_list.append((updated_edge['0'][0], updated_edge['1'][0], k))
+        else:
+            edit_list.append((updated_edge['0'][0], updated_edge['1'][0], updated_edge['2'][0]))
+
+        break
+
+    return edit_list
+
+
+def gene2protein(entity):
+    params = {'from': 'P_ENTREZGENEID',
+              'to': 'ID',
+              'format': 'tab',
+              'query': ' '.join(set([x.split('/')[-1].strip('>') for x in entity]))}
+
+    data = urllib.parse.urlencode(params).encode('utf-8')
+    request = urllib.request.Request('https://www.uniprot.org/uploadlists/', data)
+    request.add_header('User-Agent', 'Python %s' % 'tiffany.callahan@ucdenver.edu')
+    response = urllib.request.urlopen(request)
+    results = response.read()
+
+    # create dictionary mapping
+    gene_dict = {}
+
+    for res in str(results).split('\\n')[1:]:
+        i = res.split('\\t')
+        if len(i) > 1:
+            gene = 'http://purl.uniprot.org/geneid/' + str(i[0])
+            protein = 'https://www.uniprot.org/uniprot/' + str(i[1].split('_')[0])
+
+            if gene in gene_dict.keys():
+                gene_dict[gene].append(protein)
+            else:
+                gene_dict[gene] = [protein]
+
+    return gene_dict
+
+
+# read in graphs
+# graph = Graph()
+# graph.parse('./resources/ontologies/v1.0.0/doid_with_imports.owl')
+# graph.parse('./resources/ontologies/v1.0.0/hp_with_imports.owl')
+# graph.parse('./resources/ontologies/v1.0.0/go_with_imports.owl')
+
+# read in ignacio's edges (n=674675)
+it_edges = []
+for file in glob.glob('./resources/graphs/ignacio_edges/*.txt'):
+    f = open(file, 'r').read().split('\n')
+    it_edges += f
+it_edges = list(set(it_edges))
+len(it_edges)
+
+# ignacio_edge_types = {}
+# edges = set()
+#
+# for edge in filter(None, it_edges):
+#     triple = edge.strip(' ').split(' ')
+#     print(edge)
+#
+#     edges.add(triple[1])
+#
+#     if 'obo' in triple[0] and 'obo' not in triple[2]:
+#         s = triple[0].split('/')[-1].split('_')[0]
+#         if 'uniprot' in triple[2]:
+#             o = triple[2].split('/')[3]
+#         else:
+#             o = '/'.join(triple[2].split('/')[:-1])
+#     elif 'obo' not in triple[0] and 'obo' in triple[2]:
+#         if 'uniprot' in triple[0]:
+#             s = triple[0].split('/')[3]
+#         else:
+#             s = '/'.join(triple[0].split('/')[:-1])
+#         o = triple[2].split('/')[-1].split('_')[0]
+#     elif 'obo' in triple[0] and 'obo' in triple[2]:
+#         s = triple[0].split('/')[-1].split('_')[0]
+#         o = triple[2].split('/')[-1].split('_')[0]
+#     else:
+#         if 'uniprot' in triple[2]:
+#             s = triple[2].split('/')[3]
+#         else:
+#             s = '/'.join(triple[2].split('/')[:-1])
+#         if 'uniprot' in triple[0]:
+#             o = triple[0].split('/')[3]
+#         else:
+#             o = '/'.join(triple[0].split('/')[:-1])
+#
+#     if triple[1] in ignacio_edge_types:
+#         ignacio_edge_types[triple[1]] |= set([str(s) + '-' + str(o)])
+#     else:
+#         ignacio_edge_types[triple[1]] = set([str(s) + '-' + str(o)])
+#
+#
+# ignacio_edge_types['<AOPwiki_upstream_of>']
+# ignacio_edge_types['<http://purl.obolibrary.org/obo/RO_0000056>']
+# ignacio_edge_types['<http://purl.obolibrary.org/obo/RO_0000085>']
+# ignacio_edge_types['<http://purl.obolibrary.org/obo/RO_0001025>']
+# ignacio_edge_types['<http://purl.obolibrary.org/obo/RO_0002211>']
+# ignacio_edge_types['<http://purl.obolibrary.org/obo/RO_0002434>']
+# ignacio_edge_types['<http://purl.obolibrary.org/obo/RO_0002436>']
+# ignacio_edge_types['<http://www.w3.org/2000/01/rdf-schema#subClassOf>']
+
+
+# read in my edges and delete mesh terms and gene-gene interactions (n=895056)
+tc_edges = set()
+for edge in open('./resources/graphs/v1.0.0/KG_triples.txt').read().split('.\n')[:-1]:
+    triple = edge.strip(' ').split(' ')
+    if 'mesh' not in triple[0]:
+        if 'geneid' not in triple[2]:
+            tc_edges.add(edge)
+
+tc_edges = list(tc_edges)
+len(tc_edges)
+
+# convert entrez genes back to uniprot
+entity = [x for y in tc_edges for x in y.split(' ') if 'geneid' in x]
+gene_dict = gene2protein(entity)
+print(len(gene_dict))
+
+# only keep edges with valid entrez to protein mappings (n=885902)
+good_gene_edges = set()
+for x in range(0, len(tc_edges)):
+    print(str(x) + '/' + str(len(tc_edges)))
+    edge = tc_edges[x]
+    triple = edge.strip(' ').split(' ')
+    if len(triple) > 1:
+        for entity in triple:
+            if 'geneid' not in entity:
+                good_gene_edges.add(edge)
+            if 'geneid' in entity:
+                if entity.strip(' ').strip('<').strip('>') in gene_dict.keys():
+                    good_gene_edges.add(edge)
+
+            break
+
+# update list
+tc_edge_final = list(good_gene_edges)
+len(tc_edge_final)
+
+# combine edges (n=1560576)
+comb_edges = list(filter(None, [*it_edges, *tc_edge_final]))
+len(comb_edges)
+
+# combine tc and ig edges and create graph
+graph = Graph()
+AOP = Namespace('https://aopwiki.org/ext/')
+obo = Namespace('http://purl.obolibrary.org/obo/')
+oboInOwl = Namespace('http://www.geneontology.org/formats/oboInOwl#')
+dc = Namespace('http://purl.org/dc/elements/1.1/')
+hsapdv = Namespace('http://purl.obolibrary.org/obo/hsapdv#')
+owl = Namespace
+
+for edge in comb_edges:
+    triple = edge.strip(' ').split(' ')
+    updated_edge = {}
+    print(edge)
+
+    if triple[0] != triple[2]:
+        for entity in triple:
+            entity = re.sub('<|>', '', entity)
+
+            if 'geneid' in entity:
+                new = [URIRef(x) for x in gene_dict[entity.strip(' ').strip('<').strip('>')]]
+                updated_edge[str(triple.index('<' + str(entity) + '>'))] = new
+            elif 'aopwiki' in str(entity).lower():
+                new = [URIRef(str(AOP) + str(entity.split('/')[-1]))]
+                updated_edge[str(triple.index('<' + str(entity) + '>'))] = new
+            else:
+                new = [URIRef(str(entity))]
+                updated_edge[str(triple.index('<' + str(entity) + '>'))] = new
+
+        for trip in edge_loader(updated_edge):
+            graph.add(trip)
+
+# serialize graph -- write it out (n=2033803)
+graph.serialize(destination='./resources/graphs/ignacio_edges/tc_ig_combined_edges.owl', format='nt')
--- scripts/python/DataSources.py
+++ scripts/python/DataSources.py
@@ -1,41 +1,37 @@
 ##########################################################################################
 # DataSources.py
 # Purpose: script to download files and store source metadata
-# version 2.1.0
-# date: 11.12.2017
-# Python 3.6.2
 ##########################################################################################
 
 
-# import needed libraries
-from datetime import *
-import requests
-import os
+import gzip
+import io
 import os.path
+import requests
+# import subprocess
+
+from abc import ABCMeta, abstractmethod
+import datetime
+from owlready2 import *
 
 
 class DataSource(object):
-    """Class creates an object that stores different aspects of data.
-
-    The class takes as input a string that represents the file path/name of a text file that stores 1 to many
+    """The class takes as input a string that represents the file path/name of a text file that stores 1 to many
     different data sources. Each source is shown as a URL. The class is comprised of several methods that are all
     designed to perform a different function on the data sources. In the same directory that each type of data is
-    downloaded to, a metadata file is created that stores important information on each of the files that is downloaded
+    downloaded to, a metadata file is created that stores important information on each of the files that is
+    downloaded. The class has two subclasses which inherit its methods. Each subclass contains an altered version of
+    the primary classes methods that are specialized for that specific data type.
 
-        :param
-            data_path (str): a file path/name to a text file storing URLs of different sources to download
-
-            data_type (str): type of data sources
-
-            source_list (list): a list of URLs representing the data sources to download/process
-
-            data_files (list): the full file path and name of each downloaded data source
-
-    The class has two subclasses which inherit its methods. Each subclass contains an altered version of the primary
-    classes methods that are specialized for that specific data type. The subclasses can be identified in scripts
-    with the same name, followed by "_data type".
+    Attributes:
+        data_path: a string file path/name to a text file storing URLs of different sources to download.
+        data_type: a string specifying the type of data source.
+        source_list: a list of URLs containing the data sources to download/process.
+        data_files: a list of strings, which contain the full file path/name of each downloaded data source.
 
     """
+
+    __metaclass__ = ABCMeta
 
     def __init__(self, data_path):
         self.data_path = data_path
@@ -44,24 +40,19 @@ class DataSource(object):
         self.data_files = []
         self.metadata = []
 
-
-
     def get_data_type(self):
         """Function returns the input value for data type"""
-
         return self.data_type
 
-
-
     def file_parser(self):
-        """Function verifies that the file contains data and then outputs the a list where each item is a line from the
-        input text file. Prior to reading in any data the function verifies that the text file is not empty
+        """Verifies a file contains data and then outputs a list where each item is a line from the
+        input text file.
 
-        :param
-            input_file (str): file path/name to a text file containing data sources to parse
+        Returns:
+            source_list: a list, where each item represents a data source from the input text file.
 
-        :return:
-            source_list (list): each item represents a data source from the input text file
+        Raises:
+            If input file is empty an exception is raised.
 
         """
 
@@ -70,91 +61,70 @@ class DataSource(object):
         else:
             self.source_list = list(filter(None, [row.strip() for row in open(self.data_path).read().split('\n')]))
 
-
-
     def get_source_list(self):
-        """Function returns the data sources parsed from input text file"""
-
+        """Gets the data sources parsed from input text file"""
         return self.source_list
 
-
     def url_download(self):
-        """
-        Function downloads each source from a list and writes the downloaded file to a directory. Specific details
-        for the function are documented in each data-type specific subclass.
-        """
-
+        """Downloads each source from a list and writes the downloaded file to a directory."""
         pass
 
     def get_data_files(self):
-        """Function returns the list of data sources with file path"""
-
+        """Gets the list of data sources with file path"""
         return self.data_files
 
     def source_metadata(self):
-        """Function generates metadata for the data sources that it imports. Metadata includes the date of download,
+        """Extracts and stores metadata for imported data sources. Metadata includes the date of download,
         date of last modification to the file, the difference in days between last date of modification and current
         download date, file size in bytes, path to file, and URL from which the file was downloaded for each data source
 
-            :param
-                data_file (list): each item in the list contains the full path and filename of an input data source
-
-             :return:
-                 metadata (list): nested list where first item is today's date and each remaining item is a list that
-                 contains metadata information for each source that was downloaded
+        Returns:
+            metadata: a nested list, where first item is today's date and each remaining item is a list that
+            contains metadata information for each downloaded data source.
         """
 
-        self.metadata.append(['#' + str(datetime.utcnow().strftime('%a %b %d %X UTC %Y')) + ' \n'])
+        self.metadata.append(['#' + str(datetime.datetime.utcnow().strftime('%a %b %d %X UTC %Y')) + ' \n'])
 
         for i in range(0, len(self.data_files)):
             source = self.data_files[i]
-        # for i in range(0, len(data_files)):
-        #     source = data_files[i]
 
             # get vars for metadata file
             file_info = requests.head(self.source_list[i])
-            # file_info = requests.head(source_list[i])
 
-            if ".owl" in source:
-                mod_date = "NONE"
-                diff_date = "N/A"
+            if 'modified' in [x.lower() for x in file_info.headers.keys()]:
+                mod_info = file_info.headers['modified'][0]
+            elif 'Last-Modified' in [x.lower() for x in file_info.headers.keys()]:
+                mod_info = file_info.headers['Last-Modified'][0]
+            elif 'Date' in [x.lower() for x in file_info.headers.keys()]:
+                mod_info = file_info.headers['Date']
+            else:
+                mod_info = datetime.datetime.now().strftime('%a, %d %b %Y %X GMT')
 
-            if ".owl" not in source:
-                mod_info = file_info.headers[[x for x in file_info.headers.keys() if 'modified' in x.lower() or
-                                              'Last-Modified' in x.lower()][0]]
-                mod_date = datetime.strptime(mod_info, '%a, %d %b %Y %X GMT').strftime('%m/%d/%Y')
+            # reformat date
+            mod_date = datetime.datetime.strptime(mod_info, '%a, %d %b %Y %X GMT').strftime('%m/%d/%Y')
 
-                # difference in days between download date and last modified date
-                diff_date = (datetime.now() - datetime.strptime(mod_date,'%m/%d/%Y')).days
+            # difference in days between download date and last modified date
+            diff_date = (datetime.datetime.now() - datetime.datetime.strptime(mod_date, '%m/%d/%Y')).days
 
             # add metadata for each source as nested list
-            source_metadata = ['DOWNLOAD_URL= %s' % str(self.source_list[i]),
-                               'DOWNLOAD_DATE= %s' % str(datetime.now().strftime('%m/%d/%Y')),
-                               'FILE_SIZE_IN_BYTES= %s' % str(os.stat(source).st_size),
-                               'FILE_AGE_IN_DAYS= %s' % str(diff_date),
-                               'DOWNLOADED_FILE= %s' % str(source),
-                               'FILE_LAST_MOD_DATE= %s' % str(mod_date)]
+            source_metadata = ['DOWNLOAD_URL= {}'.format(str(self.source_list[i])),
+                               'DOWNLOAD_DATE= {}'.format(str(datetime.datetime.now().strftime('%m/%d/%Y'))),
+                               'FILE_SIZE_IN_BYTES= {}'.format(str(os.stat(source).st_size)),
+                               'FILE_AGE_IN_DAYS= {}'.format(str(diff_date)),
+                               'DOWNLOADED_FILE_LOCATION= {}'.format(str(source)),
+                               'FILE_LAST_MOD_DATE= {}'.format(str(mod_date))]
 
             self.metadata.append(source_metadata)
 
-
     def get_source_metadata(self):
-        """Function returns the list of data source metadata"""
-
+        """Prints a list of data source metadata"""
         return self.metadata
 
-
     def write_source_metadata(self):
-        """Function generates a text file that stores metadata for the data sources that it imports.
+        """Generates a text file that stores metadata for the data sources that it imports.
 
-        :param
-            metadata (list): nested list where first item is today's date and each remaining item is a list that
-                 contains metadata information for each source that was downloaded
-
-            data_type (str): the type of data files being processed
-
-        :return:
-            writes a text file storing metadata for the imported ontologies and names it
+        Returns:
+            Writes a text file storing metadata for the imported ontologies and names it.
 
         """
 
@@ -162,7 +132,6 @@ class DataSource(object):
         write_location = str('/'.join(self.data_files[0].split('/')[:-1]) + '/') + str('_'.join(self.data_type.split(
             '_')[:-1])) + '_metadata.txt'
         outfile = open(write_location, 'w')
-
         outfile.write(self.metadata[0][0])
         print('Writing ' + str(self.data_type) + ' metadata \n')
 
@@ -177,5 +146,136 @@ class DataSource(object):
             outfile.write(str(source[4]) + '\n')
             outfile.write(str(source[5]) + '\n')
             outfile.write('\n')
-
         outfile.close()
+
+        return None
+
+    @abstractmethod
+    def data_type(self):
+        """"A string representing the type of data being processed."""
+        pass
+
+
+class OntData(DataSource):
+
+    def data_type(self):
+        """"A string representing the type of data being processed."""
+        return 'Ontology Data'
+
+    def file_parser(self):
+        """Verifies that the file contains data and then outputs a list where each item is a line from the
+        input text file. A check is performed before processing any data to ensure that the data file is
+        not empty. The function also verifies the URL for each ontology to ensure that they are OBO/OWL ontologies
+        before processing any text.
+
+        Returns:
+            source_list: A list, where each item represents a data source from the input text file.
+
+        Raises:
+            An exception is raised if the file contains data.
+            An exception is raised if a URL does point to a source containing data.
+        """
+
+        # CHECK - file has data
+        if os.stat(self.data_path).st_size == 0:
+            raise Exception('ERROR: input file: {} is empty'.format(self.data_path))
+        else:
+            source_list = list(filter(None, [row.strip() for row in open(self.data_path).read().split('\n')]))
+
+            # CHECK - all sources have correct URL
+            valid_sources = [url for url in source_list if 'purl.obolibrary.org/obo' in url]
+
+            if len(source_list) == len(valid_sources):
+                self.source_list = source_list
+            else:
+                raise Exception('ERROR: Not all URLs were formatted properly')
+
+    def url_download(self):
+        """Takes a string representing a file path/name to a text file as an argument. The function assumes
+        that each item in the input file list is an URL to an OWL/OBO ontology. For each URL, the referenced ontology is
+        downloaded, and used as input to an OWLTools command line argument (
+        https://github.com/owlcollab/owltools/wiki/Extract-Properties-Command), which facilitates the download and
+        saving of ontologies imported by the primary ontology. The function will save the downloaded ontology + imported
+        ontologies.
+
+        Returns:
+            source_list: A list, where each item in the list represents an ontology via URL.
+
+        Raises:
+            An exception is raised if any of the URLs passed as command line arguments fails to return data.
+
+        """
+        print('\n' + '#' * 100 + '\n Downloading/Processing Ontology: {}'.format(self.data_type) + '\n')
+
+        i = 0
+        for i in range(0, len(self.source_list)):
+            source = self.source_list[i]
+            file_prefix = source.split('/')[-1].split('.')[0]
+            print('Downloading: {}'.format(str(file_prefix)))
+
+            try:
+                subprocess.check_call(['./resources/lib/owltools',
+                                       str(source),
+                                       '--merge-import-closure',
+                                       '-o',
+                                       './resources/ontologies/'
+                                       + str(file_prefix) + '_with_imports.owl'])
+
+                self.data_files.append('./resources/ontologies/' + str(file_prefix) + '_with_imports.owl')
+
+            except subprocess.CalledProcessError as error:
+                print(error.output)
+
+
+
+        # CHECK - all URLs returned a data file
+        if len(self.source_list) != i + 1:
+            raise Exception('ERROR: Not all URLs returned a data file')
+
+
+class Data(DataSource):
+
+    def data_type(self):
+        """"A string representing the type of data being processed."""
+        return 'Instance Data'
+
+    def url_download(self):
+        """Takes a string representing a file path/name to a text file as an argument. The function assumes that
+    each item in the input file list is a valid URL. For each URL, the referenced data source is downloaded and the
+    output is written to a specific directory and named according to the string included in the URL.
+
+    Returns:
+        source_list: A list, where each item in the list represents a data source.
+
+    Raises:
+        An exception is raised if any URL does point to a valid endpoint containing data.
+
+    """
+
+        print('\n' + '#' * 100 + '\n DownloadingInstance Data Source: {}'.format(str(self.data_type)) + '\n')
+
+        i = 0
+        for i in range(0, len(self.source_list)):
+            source = self.source_list[i]
+            file_prefix = source.split('/')[-1].split('.')[0]
+            print('Downloading ' + str(file_prefix))
+
+            self.data_files.append('./resources/text_files/' + str(file_prefix) + '.txt')
+
+            # verify endpoint and download data -- checks whether downloaded data is compressed
+            if '.gz' in source:
+                response = requests.get(source)
+                content = gzip.GzipFile(fileobj=io.BytesIO(response.content)).read()
+            else:
+                response = requests.get(source)
+                content = io.BytesIO(response.content).read()
+
+            # write downloaded file to directory
+            filename = './resources/text_files/' + str(file_prefix) + '.txt'
+            file = open(filename, 'w')
+            file.write(str(content))
+            file.close()
+
+        # CHECK - all URLs returned an data file
+        if len(self.source_list) != i+1:
+            raise Exception('ERROR: Not all URLs returned a data file')
