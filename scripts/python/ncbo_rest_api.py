77a902e5ca8426ef17ee454f13952c260127aa3f
callahantiff <callahantiff@gmail.com>
2020-01-27 13:24:30 -0700

    convert to use requests instead of urllib

--- scripts/python/ncbo_rest_api.py
+++ scripts/python/ncbo_rest_api.py
@@ -5,53 +5,49 @@
 # import needed libraries
 import json
 import os
+import requests
 import time
-import urllib.error
-import urllib.parse
-import urllib.request
 
 from tqdm import tqdm
-from urllib.error import HTTPError
 
 
-def gets_json_results_from_api_call(url):
+def gets_json_results_from_api_call(url: str, api_key: str):
     """Function makes API requests and returns results as a json file. API documentation can be found here:
-    http://data.bioontology.org/documentation.
+    http://data.bioontology.org/documentation. If a 500 HTTP server-side code is return from "status_code" then the
+    algorithm pauses for 30 seconds before trying the request again.
 
     Args:
-        url (str): A string containing a URL to be run against an API.
+        url: A string containing a URL to be run against an API.
+        api_key: A string containing an API key.
 
     Return:
-        A json-formatted file of API results.
+        A json-formatted file containing API results.
 
     Raises:
         An exception is raised if a 500 HTTP server-side code is raised.
+
     """
 
-    try:
+    response = requests.get(url, headers={'Authorization': 'apikey token=' + api_key})
+
+    if response.status_code == 500:
         # to ease rate limiting sleep for random amount of time between 10-60 seconds
-        opener = urllib.request.build_opener()
-
-    except HTTPError:
-        # pause for 1 minutes and try again
         time.sleep(30)
-        opener = urllib.request.build_opener()
+        response = requests.get(url, headers={'Authorization': 'apikey token=' + api_key})
 
-    # fetch data
-    opener.addheaders = [('Authorization', 'apikey token=' + open('resources/bioportal_api_key.txt').read())]
-
-    return json.loads(opener.open(url).read())
+    return json.loads(response.text)
 
 
-def writes_data_to_file(file_out, results):
+def writes_data_to_file(file_out: str, results: set):
     """Function iterates over set of tuples and writes data to text file locally.
 
     Args:
-        file_out (str): File path for location to write data to.
-        results (set): A set of tuples, where each tuple represents a mapping between two identifiers.
+        file_out: A filepath to write data to.
+        results: A set of tuples, where each tuple represents a mapping between two identifiers.
 
     Returns:
         None.
+
     """
 
     print('\n' + '=' * 50)
@@ -66,30 +62,34 @@ def writes_data_to_file(file_out, results):
     outfile.write('\n')
     outfile.close()
 
+    return None
 
-def extracts_mapping_data(source1, source2, file_out):
+
+def extracts_mapping_data(api_key: str, source1: str, source2: str, file_out: str):
     """Function uses the BioPortal API to retrieve mappings between two sources. The function batch processes the
     results in chunks of 1000, writes the data to a temporary directory and then once all batches have been
     processed, the data is concatenated into a single file.
 
     Args:
-        source1 (str): An ontology.
-        source2 (str): An ontology.
-        file_out (str): File path for location to write data to.
+        api_key: A string containing a user BiPortal API key.
+        source1: A string naming a source ontology that you want to map from.
+        source2: A string naming an ontology that you want to map identifiers from source1 to.
+        file_out: A filepath to write data to.
 
     Returns:
         None.
+
     """
 
     print('\n' + '=' * 50)
-    print('Running REST API to map {source1} to {source2}'.format(source1=source1, source2=source2))
+    print('Running REST API - Mapping: {source1} to {source2}'.format(source1=source1, source2=source2))
     print('=' * 50 + '\n')
 
     # get the available resources for mappings to source
     ont_source = 'http://data.bioontology.org/ontologies/{source}/mappings/'.format(source=source1)
-    api_results = gets_json_results_from_api_call(ont_source)
+    api_results = gets_json_results_from_api_call(ont_source, api_key)
 
-    # enable batch processing
+    # enable batch processing of api result pages
     total_pages = list(range(1, api_results['pageCount'] + 1))
     n = 500 if len(total_pages) > 5000 else 100
     batches = [total_pages[i:i + n] for i in range(0, len(total_pages), n)]
@@ -100,7 +100,7 @@ def extracts_mapping_data(source1, source2, file_out):
         # iterate over each page of results
         for page in tqdm(batches[batch]):
             time.sleep(5)
-            content = gets_json_results_from_api_call(ont_source + '?page={page}'.format(page=page))
+            content = gets_json_results_from_api_call(ont_source + '?page={page}'.format(page=page), api_key)
 
             for result in content['collection']:
                 if source2 in result['classes'][1]['links']['ontology']:
@@ -113,20 +113,26 @@ def extracts_mapping_data(source1, source2, file_out):
 
 def main():
 
+    # get api key
+    # open('resources/bioportal_api_key.txt').read()
+    api_key = input('Please provide your BioPortal API Key: ')
+
     # get user info for sources to map
-    source1 = input('Enter ontology source 1: ')
-    source2 = input('Enter ontology source 2: ')
+    source1 = input('Enter ontology source 1: ').upper()
+    source2 = input('Enter ontology source 2: ').upper()
 
     # run API call in batches + save data
-    file_path = 'resources/data_maps/'
-    file_name = '{source1}_{source2}_MAP'.format(source1=source1.upper(), source2=source2.upper())
+    file_path = './resources/processed_data/'
     temp_directory = file_path + 'temp'
 
     # create temp directory to store batches
-    os.mkdir(temp_directory)
+    if not os.path.isdir(temp_directory):
+        print('Creating a temporary directory to write API results to: {}'.format(temp_directory))
+        os.mkdir(temp_directory)
 
     # run program to map identifiers between source1 and source2
-    extracts_mapping_data(source1, source2, temp_directory + '/' + file_name)
+    file_name = '/{source1}_{source2}_MAP'.format(source1=source1, source2=source2)
+    extracts_mapping_data(api_key, source1, source2, temp_directory + file_name)
 
 
 if __name__ == '__main__':
