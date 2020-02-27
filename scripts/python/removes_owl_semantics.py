3e42781f5bac3f15b2b1779b407a922e08669b12
callahantiff <callahantiff@gmail.com>
2020-02-27 09:29:04 -0700

    removed constraint on rdlif.extras external graphs

--- scripts/python/removes_owl_semantics.py
+++ scripts/python/removes_owl_semantics.py
@@ -2,12 +2,11 @@
 # -*- coding: utf-8 -*-
 
 # import needed libraries
-import json
+import pickle
 import rdflib
 
 from networkx import networkx
 from rdflib import Graph
-from rdflib.extras.external_graph_libs import *
 from tqdm import tqdm
 from typing import Dict, Mapping, List, Optional, Set, Tuple, Union
 
@@ -16,46 +15,35 @@ class OWLNETS(object):
     """Class removes OWL semantics from an ontology or knowledge graph using the OWL-NETS method.
 
     OWL Semantics are nodes and edges in the graph that are needed in order to create a rich semantic representation
-    and to do things like run reasoners. Many of these nodes and edges  and The first step of this class
-    is to convert an input rdflib graph into a networkx multidigraph object. Once the
-    knowledge graph has been converted, it is queried to obtain all owl:Class type nodes. Using the list of classes,
-    the multidigraph is queried in order to identify
+    and to do things like run reasoners. Many of these nodes and edges  and The first step of this class is to convert
+    an input rdflib graph into a networkx multidigraph object. Once the knowledge graph has been converted, it is
+    queried to obtain all owl:Class type nodes. Using the list of classes, the multidigraph is queried in order to
+    identify
 
     Attributes:
         knowledge_graph: An RDFLib object.
+        nx_multidigraph: A networkx graph object that contains the same edges as knowledge_graph.
         uuid_map: A dictionary dictionary that stores the mapping between each class and its instance.
-        nx_multidigraph: A networkx graph object.
         write_location: A file path used for writing knowledge graph data.
         full_kg: A string containing the filename for the full knowledge graph.
         class_list: A list of owl classes from the input knowledge graph.
     """
 
-    def __init__(self, knowledge_graph: rdflib.Graph, uuid_class_map: Dict[str, str], write_location: str,
-                 full_kg: str):
+    def __init__(self, knowledge_graph: rdflib.Graph, networkx_graph: networkx.MultiDiGraph,
+                 uuid_class_map: Dict[str, str], write_location: str, full_kg: str) -> None:
+
         self.knowledge_graph = knowledge_graph
+        self.nx_multidigraph = networkx_graph
         self.uuid_map = uuid_class_map
         self.write_location = write_location
         self.full_kg = full_kg
-        self.class_list: Optional[List] = None
-
-        # uuid_class_map = 'resources/knowledge_graphs/relations_only/PheKnowLator_full_NotClosed_OWLSemantics_ClassInstanceMap.json'
-        # knowledge_graph = Graph()
-        # knowledge_graph.parse('resources/knowledge_graphs/inverse_relations/PheKnowLator_full_InverseRelations_NotClosed_OWLSemantics_KG.owl')
+        self.class_list: Optional[List[rdflib.URIRef]] = None
 
         # convert RDF graph to multidigraph (the ontology is large so this takes ~45 minutes)
-        import time
-        start = time.time()
-        print('Converting knowledge graph to MultiDiGraph. Note, this process takes ~50 minutes')
-        # nx_multidigraph: Graph = networkx.MultiDiGraph()
-        #
-        # # add edges with attributes
-        # for edge in tqdm(knowledge_graph):
-        #     nx_multidigraph.add_edge(str(edge[0]), str(edge[2]))
-        #     nx_multidigraph[str(edge[0])][str(edge[2])][0]['predicate'] = [str(edge[1])]
-
-        nx_multidigraph = rdflib_to_networkx_multidigraph(knowledge_graph)
-        end = time.time()
-        print('Total time to run code: {} seconds'.format(end - start))
+        # print('Converting knowledge graph to MultiDiGraph. Note, this process may take a while.')
+        # self.nx_multidigraph: networkx.MultiDiGraph = networkx.MultiDiGraph()
+        # for s, p, o in tqdm(knowledge_graph):
+        #     nx_multidigraph.add_edge(s, o, **{'key': p})
 
     def finds_classes(self) -> List[Union[rdflib.URIRef, rdflib.BNode]]:
         """Queries a knowledge graph and returns a list of all owl:Class objects in the graph.
@@ -84,7 +72,8 @@ class OWLNETS(object):
             raise ValueError('ERROR: No classes returned from query.')
 
     @staticmethod
-    def recurses_ontology_axioms(seen_nodes: List[rdflib.term], axioms: List[rdflib.term]) -> List[rdflib.BNode]:
+    def recurses_axioms(seen_nodes: List[rdflib.BNode],
+                        axioms: List[Union[rdflib.BNode, rdflib.URIRef, rdflib.Literal]]) -> List[rdflib.BNode]:
         """Function recursively searches a list of knowledge graph nodes and tracks the nodes it has visited. Once
         it has visited all nodes in the input axioms list, a final unique list of relevant nodes is returned. This
         list is assumed to include all necessary nodes needed to re-create an OWL equivalentClass.
@@ -112,7 +101,7 @@ class OWLNETS(object):
 
         if len(tracked_nodes) > 0:
             seen_nodes += list(set(tracked_nodes))
-            return recurses_ontology_axioms(seen_nodes, search_axioms)
+            return recurses_axioms(seen_nodes, search_axioms)
         else:
             return seen_nodes
 
@@ -152,7 +141,7 @@ class OWLNETS(object):
         # recursively loop over anonymous nodes in out_edges to create a list of relevant edges to rebuild class
         for axiom in out_edges:
             if isinstance(axiom, rdflib.BNode):
-                for element in recurses_ontology_axioms([], list(nx_multidigraph.out_edges(axiom, keys=True))):
+                for element in recurses_axioms([], list(nx_multidigraph.out_edges(axiom, keys=True))):
                     matches += list(nx_multidigraph.out_edges(element, keys=True))
 
         # create dictionary of edge lists
@@ -225,9 +214,8 @@ class OWLNETS(object):
 
     @staticmethod
     def parses_constructors(node: rdflib.URIRef, edges: Dict[str, str], class_dict: Dict[str, str],
-                            relation: rdflib.URIRef = None) -> Tuple[Set[Tuple[rdflib.URIRef, rdflib.URIRef,
-                                                                               Union[rdflib.URIRef, rdflib.Literal]]],
-                                                                     Dict[str, str]]:
+                            relation: rdflib.URIRef = None) ->\
+            Tuple[Set[Tuple[rdflib.URIRef, rdflib.URIRef, Union[rdflib.URIRef, rdflib.Literal]]], Dict[str, str]]:
         """Traverses a dictionary of rdflib objects used in the owl:unionOf or owl:intersectionOf constructors, from
         which the original set of edges used to the construct the class_node are edited, such that all owl-encoded
         information is removed. Examples of the transformations performed for these constructor types are shown below:
@@ -301,11 +289,13 @@ class OWLNETS(object):
         object_type = [x for x in edge_batch.keys() if x not in ['type', 'first', 'rest', 'onProperty']][0]
 
         if isinstance(edge_batch[object_type], rdflib.URIRef) or isinstance(edge_batch[object_type], rdflib.Literal):
+            object_node = node if object_type == 'hasSelf' else edge_batch[object_type]
+
             if len(edge_batch) == 3:
-                cleaned_classes |= {(node, edge_batch['onProperty'], edge_batch[object_type])}
+                cleaned_classes |= {(node, edge_batch['onProperty'], object_node)}
                 return cleaned_classes, None
             else:
-                cleaned_classes |= {(node, edge_batch['onProperty'], edge_batch[object_type])}
+                cleaned_classes |= {(node, edge_batch['onProperty'], object_node)}
                 return cleaned_classes, parses_anonymous_axioms(edge_batch, class_dict)
         else:
             axioms = class_dict[edge_batch[object_type]]
@@ -365,7 +355,9 @@ class OWLNETS(object):
                             cleaned_classes |= results[0]
                             edges = results[1]
                         else:
-                            print('STOP!!')
+                            # catch all other axioms -- only catching owl:onProperty with owl:oneOf
+                            # print('STOP!!')
+                            # print(node)
                             edges = None
 
                 # add results to dictionary
@@ -373,7 +365,8 @@ class OWLNETS(object):
                 cleaned_class_dict[node]['owl-decoded'] = cleaned_classes
 
         # save dictionary of cleaned classes
-        json.dump(cleaned_class_dict, open(self.write_location + self.full_kg[:-7] + '_OWLNETS_results.json', 'w'))
+        pickle.dump(cleaned_class_dict, open(write_location + full_kg[:-7] + '_OWLNETS_results.pickle', 'wb'))
+        # cleaned_class_dict = pickle.load(open(write_location + full_kg[:-7] + '_OWLNETS_results.json', 'rb'))
 
         # delete networkx graph object from memory
         del nx_multidigraph
@@ -385,7 +378,32 @@ class OWLNETS(object):
 
         return cleaned_class_dict
 
-    def removes_edges_with_owl_semantics(self):
+    def adds_cleaned_classes_to_graph(self, graph: Graph) -> Graph:
+        """Runs the OWL-NETS procedure by first finding all owl:Class nodes in the input knowledge_graph and then
+        decodes each class, returning a dictionary containing a set of decoded triples for each class. Each new
+        triple is then added to a new rdflib.graph.
+
+        Args:
+            graph: An rdflib.Graph object.
+
+        Returns:
+            An rdflib.Graph object that has been updated to include all triples from the cleaned_classes dictionary.
+        """
+
+        # run OWL-NETS
+        print('\nRunning OWL-NETS')
+        self.finds_classes()
+        cleaned_class_dict = self.cleans_owl_encoded_classes()
+
+        # add cleaned classes to graph
+        for node in tqdm(cleaned_class_dict.keys()):
+            for triple in cleaned_class_dict[node]['owl-decoded']:
+                print(triple[0], triple[1], triple[2])
+                graph.add((triple[0], triple[1], triple[2]))
+
+        return graph
+
+    def removes_edges_with_owl_semantics(self) -> Graph:
         """Filters the knowledge graph with the goal of removing all edges that contain entities that are needed to
         support owl semantics, but are not biologically meaningful. For example:
 
@@ -404,13 +422,8 @@ class OWLNETS(object):
             Reverted to: http://purl.obolibrary.org/obo/CHEBI_24505
 
         Returns:
-            An RDFlib graph that has been cleaned.
+            An RDFlib.Graph that has been cleaned.
         """
-
-        # run OWL-NETS
-        print('\nRunning OWL-NETS')
-        self.finds_classes()
-        self.cleans_owl_encoded_classes()
 
         # read in and reverse dictionary to map IRIs back to labels
         uuid_map = {val: key for (key, val) in self.uuid_map.items()}
@@ -418,31 +431,30 @@ class OWLNETS(object):
         # from those triples with URIs, remove triples that are about instances of classes
         update_graph = Graph()
 
-        for edge in tqdm(self.graph):
-            if not any(str(x) for x in edge if not str(x).startswith('http')):
+        for edge in tqdm(knowledge_graph):
+            if all(str(x) for x in edge if str(x).startswith('http')):
+
+                # look for created class-instances so they can be reversed
                 if any(x for x in edge[0::2] if str(x) in uuid_map.keys()):
                     if str(edge[2]) in uuid_map.keys() and 'ns#type' not in str(edge[1]):
                         update_graph.add((edge[0], edge[1], URIRef(uuid_map[str(edge[2])])))
                     else:
-                        update_graph.add((URIRef(uuid_map[str(edge[0])]), edge[1], edge[2]))
-                elif not any(str(x) for x in edge[0::2] if '#' in str(x)):
-                    if not any(str(x) for x in edge if ('ns#type' in str(x)) or ('PheKnowLator' in str(x))):
-                        update_graph.add(edge)
+                        update_graph.add((rdflib.URIRef(uuid_map[str(edge[0])]), edge[1], edge[2]))
+
+                # keep only those edges with nodes that do not include '#' and predicates without 'ns#type'
+                elif not any(str(x) for x in edge[0::2] if '#' in str(x)) and 'ns#type' not in str(edge[1]):
+                    update_graph.add(edge)
                 else:
+                    print(str(edge[0]) + '\t' + str(edge[1]) + '\t' + str(edge[2]))
                     pass
+
+        # add cleaned classes to graph
+        update_graph = adds_cleaned_classes_to_graph(update_graph)
 
         # print kg statistics
         edges = len(set(list(update_graph)))
         nodes = len(set([str(node) for edge in list(update_graph) for node in edge[0::2]]))
-        print('\nThe filtered KG contains: {node} nodes and {edge} edges\n'.format(node=nodes, edge=edges))
-
-        # add ontology annotations
-        update_graph_annot = self.adds_ontology_annotations(self.full_kg.split('/')[-1], update_graph)
-
-        # serialize edges
-        update_graph_annot.serialize(destination=self.write_location + self.full_kg, format='xml')
-
-        # apply OWL API formatting to file
-        self.ontology_file_formatter(self.write_location + self.full_kg)
+        print('\nFINISHED: Completed Removing OWL Semantics')
+        print('The Decoded Knowledge graph contains: {node} nodes and {edge} edges\n'.format(node=nodes, edge=edges))
 
         return update_graph
