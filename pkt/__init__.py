aa3f3f9cb45a0d248e7d979f3265892e17921c7b
callahantiff <callahantiff@gmail.com>
2020-03-13 17:29:11 -0600

    corrected import error

--- pkt/__init__.py
+++ pkt/__init__.py
@@ -2,20 +2,26 @@
 # -*- coding: utf-8 -*-
 
 
-""" PheKnowLator: Phenotype Knowledge Translator
-.. module:: pkt
-   :platform: Unix
-   :synopsis: knowledge graph construction library
+""" A Library for Automated Construction of Large Scale Heterogeneous Biomedical Knowledge Graphs
 
-.. moduleauthor:: Tiffany J. Callahan <tiffany.callahan@cuanschutz.edu>
+PheKnowLator (Phenotype Knowledge Translator), a fully automated Python 3 library explicitly designed for optimized
+construction of semantically-rich, large-scale biomedical KGs from complex heterogeneous data. The PheKnowLator
+framework provides detailed Jupyter Notebooks and scripts which greatly simplify KG construction, assisting even
+non-technical users through all steps of the build process. To accommodate a wide range of users and use cases,
+PheKnowLator has three build types (partial, full, and post-reasoner), can include inverse edges to link nodes,
+outputs KGs with and without OWL semantics (e.g. OWL-NETS), and generates KGs in several formats (e.g. triple edge
+lists, OWL API-formatted RDFXML, graph-pickled Networkx MultiDiGraph).
 
+There are two ways to run PheKnowLator:
+  1. Jupyter Notebook (main.ipynb)
+  2. Command line via argparse (Main.py)
 """
 
 __all__ = [
     'OntData',
     'LinkedData',
 
-    'EdgeList',
+    'CreatesEdgeList',
 
     'KGBuilder',
     'PartialBuild',
