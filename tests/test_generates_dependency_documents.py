fd125f420c55728d5925ad316bfc0d692ed9f3b2
callahantiff <callahantiff@gmail.com>
2020-03-14 22:45:25 -0600

    adding tests for dependency doc generation

--- tests/test_generates_dependency_documents.py
+++ tests/test_generates_dependency_documents.py
@@ -1,34 +1,77 @@
+import os.path
+import shutil
 import unittest
 
-from pkt.utils import *
+from unittest import mock
+
+from pkt import DocumentationMaker
 
 
-class TestKGUtils(unittest.TestCase):
-    """Class to test knowledge graph utility methods."""
+class TestDocumentationMaker(unittest.TestCase):
+    """Class to test dependency document builder methods."""
 
     def setUp(self):
 
-        # set some real and fake file name variables
-        self.not_string_filename = ['./data/ontologies/hp_with_imports.owl']
-        self.not_real_file_name = './data/ontologies/sop_with_imports.owl'
-        self.empty_ontology_file_location = './data/ontologies/hp_with_imports.owl'
-        self.good_ontology_file_location = './data/ontologies/so_with_imports.owl'
+        # create temporary directory to store data for testing
+        os.mkdir('./data/resources')
+
+        # initialize class
+        self.edge_maker = DocumentationMaker(1, './data/resources')
 
         return None
 
-    def test_gets_ontology_statistics(self):
-        """Tests gets_ontology_statistics method."""
+    @mock.patch('builtins.input', create=True)
+    def test_information_getter(self, mocked_input):
+        """Tests the information_getter method."""
 
-        # test non-string file name
-        self.assertRaises(TypeError, gets_ontology_statistics, self.not_string_filename)
+        mocked_input.side_effect = ['go-gene', 'one', 'go', 'http://purl.obolibrary.org/obo/go.owl', 'class-class',
+                                    'n', 't', '0;1', 'None', 'None', 'None', 'RO_0000056',
+                                    'http://purl.obolibrary.org/obo/', 'http://purl.uniprot.org/geneid/', ':;GO_;',
+                                    'http://geneontology.org/gene-associations/goa_human.gaf.gz']
 
-        # test fake file name
-        self.assertRaises(OSError, gets_ontology_statistics, self.not_real_file_name)
+        self.result = self.edge_maker.information_getter()
 
-        # test empty file
-        self.assertRaises(ValueError, gets_ontology_statistics, self.empty_ontology_file_location)
+        # create result dicts
+        resource_info = {'go-gene': ':;GO_;|class-class|RO_0000056|http://purl.obolibrary.org/obo/|http://purl'
+                                    '.uniprot.org/geneid/|n|t|0;1|None|None|None'}
+        class_info = {'go-gene': 'http://geneontology.org/gene-associations/goa_human.gaf.gz'}
+        ont_info = {'go': 'http://purl.obolibrary.org/obo/go.owl'}
 
-        # if file is good a FileNotFound error should be raised when attempting to run OWL Tools
-        self.assertRaises(FileNotFoundError, gets_ontology_statistics, self.good_ontology_file_location)
+        self.assertEqual(self.result[0], resource_info)
+        self.assertEqual(self.result[1], class_info)
+        self.assertEqual(self.result[2], ont_info)
+
+        return None
+
+    @mock.patch('builtins.input', create=True)
+    def test_writes_out_document(self, mocked_input):
+        """Tests the writes_out_document method."""
+
+        mocked_input.side_effect = ['go-gene', 'one', 'go', 'http://purl.obolibrary.org/obo/go.owl', 'class-class',
+                                    'n', 't', '0;1', 'None', 'None', 'None', 'RO_0000056',
+                                    'http://purl.obolibrary.org/obo/', 'http://purl.uniprot.org/geneid/', ':;GO_;',
+                                    'http://geneontology.org/gene-associations/goa_human.gaf.gz']
+
+        # initialize class
+        self.result = self.edge_maker.information_getter()
+
+        # write out ontology data
+        self.edge_maker.writes_out_document(self.result[0], '|', 'resource_info.txt')
+        self.assertTrue(os.path.exists('./data/resources/resource_info.txt'))
+
+        # write out class data
+        self.edge_maker.writes_out_document(self.result[1], ', ', 'class_source_list.txt')
+        self.assertTrue(os.path.exists('./data/resources/class_source_list.txt'))
+
+        # write out ontology data
+        self.edge_maker.writes_out_document(self.result[2], ', ', 'ontology_source_list.txt')
+        self.assertTrue(os.path.exists('./data/resources/ontology_source_list.txt'))
+
+        return None
+
+    def tearDown(self):
+
+        # remove temp directory
+        shutil.rmtree('./data/resources')
 
         return None
