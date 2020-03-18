20c3a2babf69392473a3acffe2c1784478a05c15
callahantiff <callahantiff@gmail.com>
2020-03-18 11:24:12 -0600

    replaced missing comma

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
                       'rdflib>=4.2.2',
                       'reactome2py>=0.0.8',
                       'requests>=2.22.0',
-                      'responses'
+                      'responses',
                       'tqdm'],
     extras_require=extras,
 )
