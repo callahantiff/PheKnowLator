bdddb74d81e6c3bc6b7d0cfc7033e52db02c0247
callahantiff <callahantiff@gmail.com>
2020-03-27 10:30:00 -0600

    commenting out dev tests

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,61 +1,61 @@
-import unittest
-
-from pkt_kg.metadata import *
-
-
-class TestMetadata(unittest.TestCase):
-    """Class to test the metadata class from the metadata script."""
-
-    def setUp(self):
-        # (self, kg_version: str, flag: str, write_location: str, kg_location: str, node_data: Optional[List],
-        # node_dict: Optional[Dict])
-
-        = Metadata()
-
-    def test_node_metadata_processor(self):
-        """Tests the node_metadata_processor method."""
-
-        return None
-
-    def test_creates_node_metadata(self):
-        """Tests the creates_node_metadata method."""
-
-        # (self, node: str, edge_type: str, url: str, graph: Graph)
-
-        return None
-
-    def test_adds_node_metadata(self):
-        """Tests the adds_node_metadata method."""
-
-        # (self, graph: Graph, edge_dict: Optional[Dict])
-
-        return None
-
-    def test_adds_annotation_assertions(self):
-        """Tests the adds_annotation_assertions method."""
-
-
-        # (graph: Graph, filename: str)
-
-        return None
-
-    def test_extracts_class_metadata(self):
-        """Tests the extracts_class_metadata data."""
-
-        # (self, graph: Graph)
-
-        return None
-
-    def test_output_knowledge_graph_metadata(self):
-        """Tests the output_knowledge_graph_metadata method."""
-
-        # (self, graph: Graph)
-
-        return None
-
-    def test_adds_ontology_annotations(self):
-        """Tests the adds_ontology_annotations method."""
-
-        # (self, filename: str, graph: Graph)
-
-        return None
+# import unittest
+#
+# from pkt_kg.metadata import *
+#
+#
+# class TestMetadata(unittest.TestCase):
+#     """Class to test the metadata class from the metadata script."""
+#
+#     def setUp(self):
+#         # (self, kg_version: str, flag: str, write_location: str, kg_location: str, node_data: Optional[List],
+#         # node_dict: Optional[Dict])
+#
+#         = Metadata()
+#
+#     def test_node_metadata_processor(self):
+#         """Tests the node_metadata_processor method."""
+#
+#         return None
+#
+#     def test_creates_node_metadata(self):
+#         """Tests the creates_node_metadata method."""
+#
+#         # (self, node: str, edge_type: str, url: str, graph: Graph)
+#
+#         return None
+#
+#     def test_adds_node_metadata(self):
+#         """Tests the adds_node_metadata method."""
+#
+#         # (self, graph: Graph, edge_dict: Optional[Dict])
+#
+#         return None
+#
+#     def test_adds_annotation_assertions(self):
+#         """Tests the adds_annotation_assertions method."""
+#
+#
+#         # (graph: Graph, filename: str)
+#
+#         return None
+#
+#     def test_extracts_class_metadata(self):
+#         """Tests the extracts_class_metadata data."""
+#
+#         # (self, graph: Graph)
+#
+#         return None
+#
+#     def test_output_knowledge_graph_metadata(self):
+#         """Tests the output_knowledge_graph_metadata method."""
+#
+#         # (self, graph: Graph)
+#
+#         return None
+#
+#     def test_adds_ontology_annotations(self):
+#         """Tests the adds_ontology_annotations method."""
+#
+#         # (self, filename: str, graph: Graph)
+#
+#         return None
--- tests/test_owlnets.py
+++ tests/test_owlnets.py
@@ -1,86 +1,86 @@
-import unittest
-
-from pkt_kg.owlnets import *
-
-
-class TestOwlNets(unittest.TestCase):
-    """Class to test the OwlNets class from the owlnets script."""
-
-    def setUp(self):
-
-        # OwlNets()
-
-        # (self, knowledge_graph: rdflib.Graph, uuid_class_map: Dict, write_location: str, full_kg: str,
-        # keep_property_types: Optional[List[str]] = None)
-
-        # class_list2 = [rdflib.URIRef('http://purl.obolibrary.org/obo/GO_0000785'),
-        #                rdflib.URIRef('http://purl.obolibrary.org/obo/CL_0000995'),
-        #                rdflib.URIRef('http://purl.obolibrary.org/obo/PATO_0000380'),
-        #                rdflib.URIRef('http://purl.obolibrary.org/obo/HP_0000340'),
-        #                rdflib.URIRef('http://purl.obolibrary.org/obo/GO_0000228'),
-        #                rdflib.URIRef('http://purl.obolibrary.org/obo/PR_000050170'),
-        #                rdflib.URIRef('http://purl.obolibrary.org/obo/CL_0000037'),
-        #                rdflib.URIRef('http://purl.obolibrary.org/obo/ENVO_01001057')]
-
-    def test_finds_classes(self):
-        """Tests the finds_classes method."""
-
-        return None
-
-    def test_removes_edges_with_owl_semantics(self):
-        """Tests the removes_edges_with_owl_semantics method."""
-
-        return None
-
-    def test_recurses_axioms(self):
-        """Tests the recurses_axioms method."""
-
-
-        # (self, seen_nodes: List[rdflib.BNode], axioms: List[Any])
-
-        return None
-
-    def test_creates_edge_dictionary(self):
-        """Tests the creates_edge_dictionary method."""
-
-        # (self, node: rdflib.URIRef)
-
-        return None
-
-    def test_returns_object_property(self):
-        """Tests the returns_object_property method."""
-
-        # (sub: rdflib.URIRef, obj: rdflib.URIRef, prop: rdflib.URIRef = None)
-
-        return None
-
-    def test_parses_anonymous_axioms(self):
-        """Tests the parses_anonymous_axioms method."""
-
-        # (edges: Dict, class_dict: Dict)
-
-        return None
-
-    def test_parses_constructors(self):
-        """Tests the parses_constructors method"""
-
-        # (self, node: rdflib.URIRef, edges: Dict, class_dict: Dict, relation: rdflib.URIRef = None)
-
-        return None
-
-    def test_parses_restrictions(self):
-        """Tests the parses_restrictions method."""
-
-        # (self, node: rdflib.URIRef, edges: Dict, class_dict: Dict)
-
-        return None
-
-    def test_cleans_owl_encoded_classes(self):
-        """Tests the cleans_owl_encoded_classes method"""
-
-        return None
-
-    def test_run_owl_nets(self):
-        """Tests the run_owl_nets method."""
-
-        return None
+# import unittest
+#
+# from pkt_kg.owlnets import *
+#
+#
+# class TestOwlNets(unittest.TestCase):
+#     """Class to test the OwlNets class from the owlnets script."""
+#
+#     def setUp(self):
+#
+#         # OwlNets()
+#
+#         # (self, knowledge_graph: rdflib.Graph, uuid_class_map: Dict, write_location: str, full_kg: str,
+#         # keep_property_types: Optional[List[str]] = None)
+#
+#         # class_list2 = [rdflib.URIRef('http://purl.obolibrary.org/obo/GO_0000785'),
+#         #                rdflib.URIRef('http://purl.obolibrary.org/obo/CL_0000995'),
+#         #                rdflib.URIRef('http://purl.obolibrary.org/obo/PATO_0000380'),
+#         #                rdflib.URIRef('http://purl.obolibrary.org/obo/HP_0000340'),
+#         #                rdflib.URIRef('http://purl.obolibrary.org/obo/GO_0000228'),
+#         #                rdflib.URIRef('http://purl.obolibrary.org/obo/PR_000050170'),
+#         #                rdflib.URIRef('http://purl.obolibrary.org/obo/CL_0000037'),
+#         #                rdflib.URIRef('http://purl.obolibrary.org/obo/ENVO_01001057')]
+#
+#     def test_finds_classes(self):
+#         """Tests the finds_classes method."""
+#
+#         return None
+#
+#     def test_removes_edges_with_owl_semantics(self):
+#         """Tests the removes_edges_with_owl_semantics method."""
+#
+#         return None
+#
+#     def test_recurses_axioms(self):
+#         """Tests the recurses_axioms method."""
+#
+#
+#         # (self, seen_nodes: List[rdflib.BNode], axioms: List[Any])
+#
+#         return None
+#
+#     def test_creates_edge_dictionary(self):
+#         """Tests the creates_edge_dictionary method."""
+#
+#         # (self, node: rdflib.URIRef)
+#
+#         return None
+#
+#     def test_returns_object_property(self):
+#         """Tests the returns_object_property method."""
+#
+#         # (sub: rdflib.URIRef, obj: rdflib.URIRef, prop: rdflib.URIRef = None)
+#
+#         return None
+#
+#     def test_parses_anonymous_axioms(self):
+#         """Tests the parses_anonymous_axioms method."""
+#
+#         # (edges: Dict, class_dict: Dict)
+#
+#         return None
+#
+#     def test_parses_constructors(self):
+#         """Tests the parses_constructors method"""
+#
+#         # (self, node: rdflib.URIRef, edges: Dict, class_dict: Dict, relation: rdflib.URIRef = None)
+#
+#         return None
+#
+#     def test_parses_restrictions(self):
+#         """Tests the parses_restrictions method."""
+#
+#         # (self, node: rdflib.URIRef, edges: Dict, class_dict: Dict)
+#
+#         return None
+#
+#     def test_cleans_owl_encoded_classes(self):
+#         """Tests the cleans_owl_encoded_classes method"""
+#
+#         return None
+#
+#     def test_run_owl_nets(self):
+#         """Tests the run_owl_nets method."""
+#
+#         return None
