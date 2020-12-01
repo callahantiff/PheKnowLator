2347923d1f1beeb9f6bca45c661bf9d9580840a9
callahantiff <callahantiff@gmail.com>
2020-12-01 10:10:59 -0700

    fixing formatting issues

--- Main.py
+++ Main.py
@@ -37,6 +37,16 @@ def main():
     # NOTE: please https://github.com/callahantiff/PheKnowLator/wiki/Dependencies page for details on how to prepare
     # input data files
 
+    import networkx as nx
+
+    # subclass - relations - owl
+    graph = "releases/v2.0.0/Manuscript_12May2020/Subclass_RelationsOnly_OWL/PheKnowLator_Subclass_RelationsOnly_NotClosed_OWL_KG_Networkx_MultiDiGraph.gpickle"
+    G = nx.read_gpickle(graph)
+    G.number_of_nodes()
+
+
+
+
     # STEP 2: PREPROCESS DATA
     # see the 'Data_Preparation.ipynb' file for instructions
 
@@ -50,7 +60,7 @@ def main():
 
     end = time.time()
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print('\nTOTAL SECONDS TO DOWNLOAD ONTOLOGIES: {} @ {}'. format(end-start, timestamp))
+    print('\nTOTAL SECONDS TO DOWNLOAD ONTOLOGIES: {} @ {}'.format(end-start, timestamp))
 
     # STEP 4: PROCESS EDGE DATA
     print('\n' + '=' * 33 + '\nDOWNLOADING DATA: CLASS DATA\n' + '=' * 33 + '\n')
