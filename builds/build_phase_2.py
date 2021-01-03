f79276a4e2dc8775904f387eddd1ebc06004b97b
callahantiff <callahantiff@gmail.com>
2021-01-03 14:10:58 -0700

    finalizing draft

--- builds/build_phase_2.py
+++ builds/build_phase_2.py
@@ -2,10 +2,13 @@
 # -*- coding: utf-8 -*-
 
 # import needed libraries
+import fnmatch
+import glob
 import os
 import shutil
 import re
 
+from datetime import date, datetime
 from google.cloud import storage
 
 from builds.data_preprocessing import DataPreprocessing
@@ -16,16 +19,15 @@ from pkt_kg.utils import data_downloader
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'resources/project_keys/pheknowlator-6cc612b4cbee.json'
 
 
-def updates_dependency_documents(bucket_url, file_url, bucket, gcs_loc, temp_directory):
+def updates_dependency_documents(gcs_url, file_url, bucket, temp_directory):
     """Takes a dependency file url and downloads the file it points to. That file is then iterated over and all urls
     are updated to point their current Google Cloud Storage bucket. The updated file is saved locally and pushed to
     the processed_data directory of the current build.
 
     Args:
-        bucket_url: A string containing a URL to the current Google Cloud Storage bucket.
+        gcs_url: A string containing a URL to the current Google Cloud Storage bucket.
         file_url: A string containing a URL to a build dependency document.
         bucket: A storage Bucket object specifying a Google Cloud Storage bucket.
-        gcs_loc: A string specifying the location of the processed_data directory for a specific build.
         temp_directory: A local directory where preprocessed data is stored.
 
     Returns:
@@ -34,25 +36,27 @@ def updates_dependency_documents(bucket_url, file_url, bucket, gcs_loc, temp_dir
 
     # set bucket information
     bucket_path = '/'.join(bucket_url.split('/')[4:])
-    original_bucket = [_.name.split('/')[-1] for _ in bucket.list_blobs(prefix=bucket_path + 'original_data')]
-    processed_bucket = [_.name.split('/')[-1] for _ in bucket.list_blobs(prefix=bucket_path + 'processed_data/')]
+    gcs_processed = [_.name.split('/')[-1] for _ in bucket.list_blobs(prefix=bucket_path + 'processed_data/')]
 
     # download dependency file and read in data
-    data_downloader(file_url, temporary_directory + '/')
+    data_downloader(file_url, temp_directory + '/')
     filename = file_url.split('/')[-1]
     data = open(temp_directory + '/' + filename).readlines()
 
     # update file with current gcs bucket url
-    with open(temp_directory + '/' + filename, 'w') as out:
-        for row in data:
-            prefix, suffix = row.split(', ')
-            data_file = suffix.split('/')[-1].strip('?dl=1')
-            if data_file in original_bucket: out.write(prefix + ', ' + bucket_url + 'original_data/' + data_file)
-            if data_file in processed_bucket: out.write(prefix + ', ' + bucket_url + 'processed_data/' + data_file)
+    if filename != 'resource_info.txt':
+        with open(temp_directory + '/' + filename, 'w') as out:
+            for row in data:
+                prefix, suffix = row.strip('\n').split(', ')
+                if '.owl' not in suffix:
+                    d_file = suffix.split('/')[-1].strip('?dl=1')
+                    if d_file in gcs_processed: out.write(prefix + ', ' + gcs_url + 'processed_data/' + d_file + '\n')
+                if '.owl' in suffix:
+                    d_file = fnmatch.filter(gcs_processed, suffix.split('/')[-1].replace('.', '*.'))
+                    if len(d_file) > 0: out.write(prefix + ', ' + gcs_url + 'processed_data/' + d_file[0] + '\n')
 
     # push data to bucket
-    blob = bucket.blob(gcs_loc + filename)
-    blob.upload_from_filename(temp_directory + '/' + filename)
+    lod_data.uploads_data_to_gcs_bucket(d_file)
 
     return None
 
@@ -80,13 +84,12 @@ def get_file_metadata(url, file_location, gcs_url):
     return metadata
 
 
-def writes_metadata(metadata, bucket, processed_data, temp_directory):
+def writes_metadata(metadata, temp_directory):
     """Writes the downloaded URL metadata information locally and to the original_data directory in Google Cloud
     Storage bucket for the current build.
 
     Args:
-        bucket: A storage Bucket object specifying a Google Cloud Storage bucket.
-        processed_data: A string specifying the location of the processed_data directory for a specific build.
+        metadata: A nested list containing metadata information on each downloaded url.
         temp_directory: A local directory where preprocessed data is stored.
         metadata: A nested list containing metadata information on each downloaded url.
 
@@ -94,20 +97,21 @@ def writes_metadata(metadata, bucket, processed_data, temp_directory):
         None.
     """
 
-    filename = 'downloaded_build_metadata.txt'
-
+    filename = 'preprocessed_build_metadata.txt'
     with open(temp_directory + '/' + filename, 'w') as out:
         out.write('=' * 35 + '\n{}\n'.format(str(datetime.utcnow().strftime('%a %b %d %X UTC %Y'))) + '=' * 35 + '\n\n')
         for row in metadata:
             for i in range(4):
                 out.write(str(row[i]) + '\n')
 
-    uploads_data_to_gcs_bucket(bucket, processed_data, temp_directory, filename)
+    lod_data.uploads_data_to_gcs_bucket(filename)
 
     return None
 
 
 def main():
+
+    print('#' * 35 + '\nBUILD PHASE 2: DATA PRE-PROCESSING\n' + '#' * 35)
 
     # create temp directory to use locally for writing data GCS data to
     temp_dir = 'builds/temp'
@@ -124,6 +128,7 @@ def main():
     build = 'build_' + sorted([x[0] for x in [re.findall(r'(?<=_)\d.*', x) for x in bucket_files] if len(x) > 0])[-1]
     gcs_original_data = '{}/{}/data/{}'.format(release, build, 'original_data/')
     gcs_processed_data = '{}/{}/data/{}'.format(release, build, 'processed_data/')
+    gcs_url = 'https://storage.googleapis.com/pheknowlator/{}/{}/data/'.format(release, build)
 
     ###############################################
     # STEP 2 - PREPROCESS BUILD DATA
@@ -132,28 +137,30 @@ def main():
 
     ###############################################
     # STEP 3 - CLEAN ONTOLOGY DATA
-    ontology_data = OntologyCleaner()
-    ontology_data.cleans_ontology_data()
+    ont_data = OntologyCleaner(bucket, gcs_original_data, gcs_processed_data, temp_dir)
+    ont_data.cleans_ontology_data()
 
     ###############################################
-    # STEP 4 - UPDATE INPUT DEPENDENCY DOCUMENTS
-    gcs_url = 'https://storage.googleapis.com/pheknowlator/{}/{}/data/'.format(release, build)
-    # edge source list
-    edge_src_list = ' https://raw.githubusercontent.com/callahantiff/PheKnowLator/master/resources/edge_source_list.txt'
-    updates_dependency_documents(gcs_url, edge_src_list, bucket, gcs_processed_data, temp_dir)
-    # ontology source list
-    ont_list = ' https://raw.githubusercontent.com/callahantiff/PheKnowLator/master/resources/ontology_source_list.txt'
-    updates_dependency_documents(gcs_url, ont_list, bucket, gcs_processed_data, temp_dir)
-
-    ###############################################
-    # STEP 5 - GENERATE METADATA DOCUMENTATION
+    # STEP 4 - GENERATE METADATA DOCUMENTATION
     metadata, processed_data = [], glob.glob(temp_dir + '/*')
 
     for data_file in tqdm(processed_data):
         url = gcs_url + '/original_data/' + data_file.replace(temp_dir + '/', '')
         metadata += [get_file_metadata(url, data_file, gcs_url + 'processed_data/')]
 
-    writes_metadata(metadata, bucket, gcs_processed_data, temp_dir)
+    writes_metadata(metadata, temp_dir)
+
+    ###############################################
+    # STEP 5 - UPDATE INPUT DEPENDENCY DOCUMENTS
+    # edge source list
+    edge_src_list = ' https://raw.githubusercontent.com/callahantiff/PheKnowLator/master/resources/edge_source_list.txt'
+    updates_dependency_documents(gcs_url, edge_src_list, bucket, temp_dir)
+    # ontology source list
+    ont_list = ' https://raw.githubusercontent.com/callahantiff/PheKnowLator/master/resources/ontology_source_list.txt'
+    updates_dependency_documents(gcs_url, ont_list, bucket, temp_dir)
+    # resource info
+    resources = 'https://raw.githubusercontent.com/callahantiff/PheKnowLator/master/resources/resource_info.txt'
+    updates_dependency_documents(gcs_url, resources, bucket, temp_dir)
 
     shutil.rmtree(temp_dir)  # clean up environment after uploading all processed data
 
