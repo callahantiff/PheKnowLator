603efd9a61a066d158d8275baf05f54e0b01f689
callahantiff <callahantiff@gmail.com>
2021-01-19 22:35:35 -0700

    removed google dep

--- builds/job_monitoring.py
+++ builds/job_monitoring.py
@@ -4,15 +4,12 @@
 # import needed libraries
 import click
 import datetime
-import json
 import os
+import re
+import subprocess
 import time
 
 from datetime import datetime
-from dateutil import parser
-from googleapiclient import discovery
-from googleapiclient import errors
-from oauth2client.client import GoogleCredentials
 
 
 @click.command()
@@ -22,15 +19,15 @@ def main(project_name, job_name):
     # tracks job status (source: https://cloud.google.com/ai-platform/training/docs/monitor-training)
 
     # get python representation of the REST API and specify project information
-    cloud_ml = discovery.build('ml', 'v1')
     job_id = 'projects/{}/jobs/{}'.format(project_name, job_name)
 
     # form the request and listen until completion
-    start_time, state = datetime.now(), 'RUNNING'
-    while state == 'RUNNING':
+    start_time, state = datetime.now(), 'QUEUED'
+    while state in ['RUNNING', 'QUEUED']:
         time.sleep(60)
-        request = cloud_ml.projects().jobs().get(name=job_id).execute()
-        state, current_time = request['state'], str(datetime.now().strftime('%b %d %Y %I:%M%p'))
+        current_time = str(datetime.now().strftime('%b %d %Y %I:%M%p'))
+        output = subprocess.check_output("gcloud ai-platform jobs describe {}".format(job_id), shell=True)
+        state = re.findall(r'state.*(?=trainingInput)', str(output))[0].strip('state: ').strip('\\n')
         elapsed_minutes = round((datetime.now() - start_time).total_seconds()/60, 3)
         print('Job Status: {} @ {} -- Elapsed Time (min): {}'.format(state, current_time, elapsed_minutes))
 
