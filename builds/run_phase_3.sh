8f9e0e21423471c4f38a73b6445596f62cfa7503
callahantiff <callahantiff@gmail.com>
2021-01-21 19:05:32 -0700

    adding auth to bash script

--- builds/run_phase_3.sh
+++ builds/run_phase_3.sh
@@ -2,6 +2,9 @@
 
 printf '%s %s Some log message \n' $(date +%Y-%m-%d) $(date +%H:%M:%S) >> '{absolute_path}/startup_script.log'
 
+# make sure authentication is set
+gcloud --quiet auth configure-docker
+
 # call build script
 cd /PheKnowLator
 python build_phase_3.py --app "$1" --rel "$2" --owl "$3"
