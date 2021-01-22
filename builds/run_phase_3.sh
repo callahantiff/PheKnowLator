6ee6da8693e4cb1a2077f996fc8adaa3e8a5d373
callahantiff <callahantiff@gmail.com>
2021-01-21 20:22:29 -0700

    trying another approach

--- builds/run_phase_3.sh
+++ builds/run_phase_3.sh
@@ -5,6 +5,11 @@ printf '%s %s Some log message \n' $(date +%Y-%m-%d) $(date +%H:%M:%S) >> '{abso
 # make sure authentication is set
 gcloud --quiet auth configure-docker
 
+# pull docker container
+pull docker "$0"
+
 # call build script
-cd /PheKnowLator
-python build_phase_3.py --app "$1" --rel "$2" --owl "$3"
+#cd /PheKnowLator
+#python build_phase_3.py --app "$1" --rel "$2" --owl "$3"
+
+docker run "$0" sh -c /PheKnowLator/build_phase_3.py --app "$1" --rel "$2" --owl "$3"
