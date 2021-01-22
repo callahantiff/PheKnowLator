c3aeb8fb21e3b95d95ca42d814d5cff4c1da33a6
callahantiff <callahantiff@gmail.com>
2021-01-21 21:17:33 -0700

    changing approach

--- builds/run_phase_3.sh
+++ builds/run_phase_3.sh
@@ -2,14 +2,6 @@
 
 printf '%s %s Some log message \n' $(date +%Y-%m-%d) $(date +%H:%M:%S) >> '{absolute_path}/startup_script.log'
 
-# make sure authentication is set
-gcloud --quiet auth configure-docker
-
-# pull docker container
-pull docker "$0"
-
 # call build script
-#cd /PheKnowLator
-#python build_phase_3.py --app "$1" --rel "$2" --owl "$3"
-
-docker run "$0" sh -c /PheKnowLator/build_phase_3.py --app "$1" --rel "$2" --owl "$3"
+cd /PheKnowLator
+python build_phase_3.py --app "$1" --rel "$2" --owl "$3"
