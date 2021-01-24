22f1134ba8d786572144b2c3e2b3ce609d7f847d
callahantiff <callahantiff@gmail.com>
2021-01-23 21:53:58 -0700

    remove uneeded scripts

--- builds/entrypoints/phase3_job1.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-printf '%s %s PKT: Some log message \n' $(date +%Y-%m-%d) $(date +%H:%M:%S)
-
-# call build script
-cd /PheKnowLator
-python build_phase_3.py --app subclass --rel no --owl no
