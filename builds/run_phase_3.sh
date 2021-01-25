14885388dded29c8725681151aff8c2a9e7f1c63
callahantiff <callahantiff@gmail.com>
2021-01-25 01:41:06 -0700

    removed uneeded script

--- builds/run_phase_3.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-printf '%s %s Some log message \n' $(date +%Y-%m-%d) $(date +%H:%M:%S) >> '{absolute_path}/startup_script.log'
-
-# call build script
-cd /PheKnowLator
-python build_phase_3.py --app "$1" --rel "$2" --owl "$3"
