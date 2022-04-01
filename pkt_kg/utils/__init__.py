5be80375e20e0fc949a310e18f08818cc03eb22f
callahantiff <callahantiff@gmail.com>
2022-04-01 12:16:12 -0400

    alphabetizing

--- pkt_kg/utils/__init__.py
+++ pkt_kg/utils/__init__.py
@@ -6,14 +6,15 @@ from .data_utils import *
 from .kg_utils import *
 
 
-__all__ = ['url_download', 'ftp_url_download', 'gzipped_ftp_url_download', 'zipped_url_download',
-           'gzipped_url_download', 'data_downloader', 'explodes_data', 'chunks', 'metadata_dictionary_mapper',
-           'metadata_api_mapper', 'genomic_id_mapper', 'outputs_dictionary_data', 'obtains_entity_url',
-           'gets_biolink_information', 'gets_ontology_statistics', 'gets_ontology_classes', 'load_jsonl', 'dump_jsonl',
-           'gets_deprecated_ontology_classes', 'gets_object_properties', 'gets_ontology_class_dbxrefs',
-           'gets_ontology_class_synonyms', 'merges_ontologies', 'ontology_file_formatter', 'adds_edges_to_graph',
-           'remove_edges_from_graph', 'gets_entity_ancestors', 'connected_components', 'removes_self_loops',
-           'derives_graph_statistics', 'splits_knowledge_graph', 'adds_namespace_to_bnodes',
-           'removes_namespace_from_bnodes', 'updates_pkt_namespace_identifiers', 'finds_node_type',
-           'updates_graph_namespace', 'maps_ids_to_integers', 'n3', 'appends_to_existing_file',
-           'deduplicates_file', 'merges_files', 'convert_to_networkx', 'sublist_creator', 'gets_ontology_definitions']
+__all__ = ['adds_edges_to_graph', 'adds_namespace_to_bnodes', 'appends_to_existing_file', 'chunks',
+           'connected_components', 'convert_to_networkx', 'data_downloader', 'deduplicates_file',
+           'derives_graph_statistics', 'dump_jsonl', 'explodes_data', 'finds_node_type', 'ftp_url_download',
+           'genomic_id_mapper', 'gets_biolink_information', 'gets_deprecated_ontology_classes',
+           'gets_entity_ancestors', 'gets_object_properties', 'gets_ontology_class_dbxrefs',
+           'gets_ontology_class_synonyms', 'gets_ontology_classes', 'gets_ontology_definitions',
+           'gets_ontology_statistics', 'gzipped_ftp_url_download', 'gzipped_url_download', 'load_jsonl',
+           'maps_ids_to_integers', 'merges_files', 'merges_ontologies', 'metadata_api_mapper',
+           'metadata_dictionary_mapper', 'n3', 'obtains_entity_url', 'ontology_file_formatter',
+           'outputs_dictionary_data', 'remove_edges_from_graph', 'removes_namespace_from_bnodes', 'removes_self_loops',
+           'splits_knowledge_graph', 'sublist_creator', 'updates_graph_namespace', 'updates_pkt_namespace_identifiers',
+           'url_download', 'zipped_url_download']
